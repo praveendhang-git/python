5365e134c49cb45527e5a234910a9c7a51b1331e
prdh0622 <praveen.dhang@netcracker.com>
2025-07-22 19:32:18 +0530

    file operation added

---
+++ File_operation.py
@@ -0,0 +1,16 @@
+import os
+file = open('IDB_mapp.txt', '+rt')
+print(file.read())
+file.close()
+# Appending a new line to the file
+with open('IDB_mapp.txt', 'a') as file:
+    file.write('\nThis is a new line added to the file.')
+# Check if the file exists
+if os.path.exists('BPI_TEST_DATA.Xlsx'):
+    print("File exists.")
+    os.remove('BPI_TEST_DATA.Xlsx')  # Remove the file
+else:
+    print("File does not exist.")
+# Creating a new file and writing to it
+with open('new_test_file.txt', 'w') as file:
+    file.write('This is a new file created and written to.')
